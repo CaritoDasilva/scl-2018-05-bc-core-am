61222d2c9d1994837497452421f2f59c234e1a33
CaritoDasilva <caritodasilva@gmail.com>
2018-05-25 15:10:06 -0400

    subiendo mi commit tardío menos mal no se quemo mi pc!

--- src/index.js
+++ src/index.js
@@ -1,1 +1,29 @@
-/* Acá va tu código */
+window.onload = function () {
+    /*BOTONES*/
+
+    const nameBtn = document.getElementById("aceptaName");
+    const claveBtn = document.getElementById("cifradoAceptar");
+    const cifrarBtn = document.getElementById("enviarMensaje");
+    const decoBtn = document.getElementById("enviarMensaje2");
+
+
+    let txtCifrado = document.getElementById("cifrado").value;
+    let secretMsn = document.getElementById("mensajeSecreto").value;
+    let cifradoMsn = document.getElementById("mensajeCifrado").value;
+
+    /*Lista de Invitados (Array)*/
+
+    let guests = ["Carolina", "Valeska", "Barbara", "Evelyn", "CarolinaD", "Marcia", "Alexandra"];
+    let welcomeTXT = document.getElementById("welcome");
+
+
+    /*Voy a llamar al botón de nombre de usuario*/
+    nameBtn.addEventListener("click", function() {
+        let welcomeTXT = document.getElementById("welcome");
+        let nameUser = document.getElementById("inputName").value;
+        welcomeTXT.innerHTML = "Hol@ " + nameUser;
+        
+    
+    });
+
+}
