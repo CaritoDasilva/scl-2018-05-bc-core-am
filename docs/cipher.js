cdaebf5ca274d9cee9696fb41535030e46997df0
CaritoDasilva <caritodasilva@gmail.com>
2018-06-01 09:41:39 -0400

    Presentación final

---
+++ docs/cipher.js
@@ -0,0 +1,29 @@
+window.cipher = {
+  encode: (secretMsn, txtCifrado) => {
+    let message = "";
+    for (let i = 0; i < secretMsn.length; i++) {
+      let ascii = ((secretMsn.charCodeAt(i) - 65 + parseInt(txtCifrado)) % 26 + 65);
+      if (secretMsn.charCodeAt(i) === 32) {
+        ascii = " ";
+        message += " ";
+      }
+      message += String.fromCharCode(ascii);
+    }
+    return message;
+  },
+  decode: (cifradoMsn, txtCifrado) => {
+    let messageReturn = "";
+    for (let i = 0; i < cifradoMsn.length; i++) {
+      let alphabet = ((cifradoMsn.charCodeAt(i) + 65 - parseInt(txtCifrado)) % 26 + 65);
+      if (cifradoMsn.charCodeAt(i) === 32) {
+        alphabet = " ";
+        messageReturn += " ";
+      }
+      messageReturn += String.fromCharCode(alphabet);
+
+    }
+    return messageReturn;
+
+
+  }
+}
---
+++ docs/index.js
@@ -0,0 +1,69 @@
+window.onload = function () {
+	/*BOTONES*/
+
+	const nameBtn = document.getElementById("aceptaName");
+	const claveBtn = document.getElementById("cifradoAceptar");
+	const cifrarBtn = document.getElementById("enviarMensaje");
+	const decoBtn = document.getElementById("enviarMensaje2");
+
+
+	let txtCifrado = document.getElementById("cifrado").value;
+	let secretMsn = document.getElementById("mensajeSecreto").value.toUpperCase();
+	let cifradoMsn = document.getElementById("mensajeCifrado").value;
+	let nameUser = document.getElementById("inputName").value;
+
+
+	/*Voy a llamar al botón de nombre de usuario*/
+
+	nameBtn.addEventListener("click", () => {
+		let welcomeTXT = document.getElementById("welcome");
+		let nameUser = document.getElementById("inputName").value;
+		let nameUser2 = document.getElementById("inputName");
+		const questionName = document.getElementById("qname");
+        
+    /*aquí le digo al botón que después que se haga click desaparezca para que solo me quede el nombre*/
+		nameBtn.style.display = "none";
+		nameUser2.style.display = "none";
+		questionName.style.display = "none";
+		welcomeTXT.innerHTML = "Bienvenid@ " + nameUser;
+	});
+
+    /*Voy a llamar al botón de la clave (número de posicionamiento)*/
+    claveBtn.addEventListener("click", () => {
+      let txtCifrado = document.getElementById("cifrado").value;
+      let txtCifrado2 = document.getElementById("cifrado");
+      const wordClave = document.getElementById("clave");
+      txtCifrado2.style.display = "none";
+      claveBtn.style.display = "none";
+      wordClave.style.display = "none";
+    });
+
+	/*Aquí voy a llamar el boton que cifra el mensaje para ponerlo funcional*/
+	cifrarBtn.addEventListener("click", () => {
+		let secretMsn = document.getElementById("mensajeSecreto").value.toUpperCase();
+		let secretMsn2 = document.getElementById("mensajeSecreto");
+		let txtCifrado = document.getElementById("cifrado").value;
+		const wordMsn1 = document.getElementById("msn1");
+		let txtCifrado2 = document.getElementById("cifrado");
+		secretMsn2.style.display = "none";
+		cifrarBtn.style.display = "none";
+		wordMsn1.style.display = "none";
+		let finalMessage = window.cipher.encode(secretMsn, txtCifrado);
+		document.getElementById("mensajeCodificado").innerHTML = "<h2>Tu mensaje cifrado:</h2>" + finalMessage;
+	});
+
+	/*Aquí voy a llamar al botón que descifra el mensaje*/
+	decoBtn.addEventListener("click", () => {
+		let cifradoMsn = document.getElementById("mensajeCifrado").value;
+		let cifradoMsn2 = document.getElementById("mensajeCifrado");
+		let txtCifrado = document.getElementById("cifrado").value;
+		const wordMsn2 = document.getElementById("msn2");
+		cifradoMsn2.style.display = "none";
+		decoBtn.style.display = "none";
+		wordMsn2.style.display = "none";
+		let finalMessage2 = window.cipher.decode(cifradoMsn, txtCifrado);
+		document.getElementById("mensajeDecodificado").innerHTML = "<h2>Tu mensaje descifrado:</h2>" + finalMessage2;
+	});
+
+
+}
